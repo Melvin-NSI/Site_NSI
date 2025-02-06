8d6f2216cea46a9de4a7d7fa55297665066d73ee
Melvin-NSI <Ze.Weshman@gmail.com>
2025-02-06 14:34:53 +0100

    ok

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-const backButton = document.createElement('button');
-backButton.textContent = 'Retour au menu principal';
-backButton.id = 'backButton';
+const backButton = document.getElementById("home")
 backButton.style.position = 'fixed';
 backButton.style.top = '10px';
 backButton.style.left = '10px';
 backButton.class = "btn";
-document.body.appendChild(backButton);
 
 backButton.onclick=function(){
     location.href="https://melvin-nsi.github.io/Site_NSI/"
-    };
+};
