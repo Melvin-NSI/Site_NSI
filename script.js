12b69ddbd74f57f45326f4022720f001879234b1
Melvin-NSI <Ze.Weshman@gmail.com>
2025-02-06 14:26:16 +0100

    btn

--- script.js
+++ script.js
@@ -4,21 +4,9 @@ backButton.id = 'backButton';
 backButton.style.position = 'fixed';
 backButton.style.top = '10px';
 backButton.style.left = '10px';
+backButton.class = "btn";
 document.body.appendChild(backButton);
 
-function scrollToTop() {
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-    });
-}
-
-backButton.onclick=function(){location.href="https://melvin-nsi.github.io/Site_NSI/"};
-
-window.onscroll = function() {
-    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
-        backButton.style.display = 'block';
-    } else {
-        backButton.style.display = 'none';
-    }
-};
+backButton.onclick=function(){
+    location.href="https://melvin-nsi.github.io/Site_NSI/"
+    };
